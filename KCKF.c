c10ee983a6f22c014c885cb057ede9e9b5dd6b9f
Hiroshi <hiroshi.wong@gmail.com>
2025-05-26 11:17:23 -0700

    Create KCKF.c

---
+++ KCKF.c
@@ -0,0 +1,21 @@
+*
+*
+*
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	const char CELSIUS = 'C';
+    //const char FAHRENHEIT = 'F';
+    const double ABS_ZERO_IN_CELSIUS = 
+    
+    char choice = CELSIUS;
+    float celTemp = 0.0;
+    float fahTemp = 0.0;
+    float kelTemp = 0.0;
+    
+    
+
+}
