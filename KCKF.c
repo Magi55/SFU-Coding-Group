547760a55021b0e5564e4d80bce9cedcf7f8ba36
Hiroshi <hiroshi.wong@gmail.com>
2025-05-26 11:24:56 -0700

    Update KCKF.c

--- KCKF.c
+++ KCKF.c
@@ -16,6 +16,31 @@ int main(void)
     float fahTemp = 0.0;
     float kelTemp = 0.0;
     
-    
+    printf("To convert temperature to Celsius ... Enter C\n");
+    printf("To convert temperature to Fahrenheit ... Enter F\n");
+    printf("Your Choice: ");
+    scanf("%c", &choice);
 
+	printf("Please, enter a Kelvin temperature: ");
+    scanf("%f", kelTemp);
+
+	if(choice == CELSIUS)
+    {
+        //convert fahTemp to celTemp
+        celTemp = kelTemp + ABS_ZERO_IN_CELSIUS;
+        
+        printf("%0.1f Kelvin => %0.1f Celcius\n",kelTemp, celTemp);
+    }
+    
+    else
+    {
+        //assume choice == FAHRENHEIT
+        
+        //convert celTemp to fahTemp
+        fahTemp = (kelTemp + ABS_ZERO_IN_CELSIUS) * 9.0/5.0 + 32.0;
+        
+        printf("%0.1f Kelvin => %0.1f Fahrenheit\n",kelTemp, fahTemp);
+    }
+    
+    return 0;
 }
