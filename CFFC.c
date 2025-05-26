e3b9b671530afc6ba1f903e490a7354a7c8c6270
Hiroshi <hiroshi.wong@gmail.com>
2025-05-26 11:06:15 -0700

    Update CFFC.c

--- CFFC.c
+++ CFFC.c
@@ -13,14 +13,42 @@
 
 int main(void)
 {
-    char choice = CELCIUS;
+    const char CELSIUS = 'C'
+    char choice = CELSIUS;
     float celTemp = 0.0;
     float fahTemp = 0.0;
     
 	printf("To convert temperature from Celsius to Fahrenheit ... Enter F\n");
     printf("To convert temperature from Fahrenheit to Celsius ... Enter C\n");
     printf("Your Choice: ");
+    scanf("%c", &choice);
     
+    if( choice == CELSIUS)
+    {
+    // F to C
+    printf("Please, enter a temperature in Fahrenheit: ");
+    scanf("%f", &fahTemp);
     
+    //convert F to C
+    celTemp = fahTemp * 5/9 - 32;
     
+    printf(" %.1f Fahrenheit => %.1f Celsius\n", fahTemp, celTemp);
+    }
+    
+    else
+    {
+    // C to F
+    printf("Please, enter a temperature in Celsius: ");
+   
+    //Read the user input
+    scanf("%f", &celTemp);
+    
+    //Convert C to F
+    fahTemp = 32 + (float) 9/5 * celTemp;
+    
+    //Print results
+    printf(" %.1f Celcius => %.1f Fahrenheit\n", celTemp, fahTemp);
+    }
+    
+    return 0;
 }
