8e615d5150b3c0b5a20ab08294a66a115b358f53
Hiroshi <hiroshi.wong@gmail.com>
2025-05-26 10:49:39 -0700

    Update CFFC.c

--- CFFC.c
+++ CFFC.c
@@ -13,7 +13,14 @@
 
 int main(void)
 {
-	
+    char choice = CELCIUS;
+    float celTemp = 0.0;
+    float fahTemp = 0.0;
+    
+	printf("To convert temperature from Celsius to Fahrenheit ... Enter F\n");
+    printf("To convert temperature from Fahrenheit to Celsius ... Enter C\n");
+    printf("Your Choice: ");
+    
     
     
 }
